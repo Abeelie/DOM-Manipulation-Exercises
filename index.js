83062352101bc92113a13e50ab1900e34bb0bb8d
Abeelie <80470256+Abeelie@users.noreply.github.com>
2021-04-03 11:03:52 -0400

    comitting files

---
+++ index.js
@@ -0,0 +1,28 @@
+// document.getElementById("container");
+// document.querySelector("container");
+// document.querySelectorAll(".second");
+// document.querySelector("ul .third");
+//
+// let new_text = document.querySelector("#container");
+// new_text.innerText = "Hello";
+//
+// let footer = document.querySelector(".footer");
+//
+// footer.classList.add("main");
+//
+// footer.classList.remove("main");
+//
+// let new_List = document.createElement("li");
+//
+// new_List.innerText = "four";
+//
+// let append_list = document.querySelector("ul");
+// append_list.appendChild(new_List);
+
+// let list = document.querySelectorAll("ol li");
+//
+// for(let i = 0; i < list.length; i++){
+//     list[i].style.backgroundColor = "green";
+// }
+//
+// footer.remove();
